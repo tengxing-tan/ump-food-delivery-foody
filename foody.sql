5cb45c4d489b2749fd80425556e5202d47243c09
ArdianaBalqis <adrianazhar123@gmail.com>
2022-05-30 09:55:09 +0800

    crud function

--- foody.sql
+++ foody.sql
@@ -23,3 +23,32 @@ CREATE TABLE `riderreport` (
   `commission_ID` int(11) NOT NULL,
   FOREIGN KEY (`commission_ID`) REFERENCES commission(`commission_ID`)
 )
+CREATE TABLE `Admin` (
+  `admin_ID` int(10) NOT NULL,
+  `user_ID` varchar(10) NOT NULL,
+  `admin_name` varchar(30) NOT NULL,
+  `admin_email` varchar(30) NOT NULL,
+  `admin_password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `User` (
+  `user_ID` int(10) NOT NULL,
+  `user_name` varchar(30) NOT NULL,
+  `user_email` varchar(30) NOT NULL,
+  `user_phoneNum` varchar(11) NOT NULL,
+  `user_password` varchar(20) NOT NULL,
+  `user_address` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `Admin`
+  ADD PRIMARY KEY (`admin_ID`),
+  ADD KEY `user_ID` (`user_ID`);
+
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`user_ID`);
+
+
+ALTER TABLE `Admin`
+  ADD CONSTRAINT `Admin_ibfk_1` FOREIGN KEY (`user_ID`) REFERENCES `User` (`user_ID`);
+
+COMMIT;
