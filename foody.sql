887c1f335f809a67b43a306ed2471b296ce8f0d8
JeremyLoh00 <82555139+JeremyLoh00@users.noreply.github.com>
2022-05-30 10:11:10 +0800

    Update foody.sql

--- foody.sql
+++ foody.sql
@@ -1,13 +1,4 @@
 
-CREATE TABLE `commission` (
-  `commission_ID` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `commission_date` date DEFAULT NULL,
-  `commission_amount` float(11) DEFAULT NULL,
-  `commission_total` float(11) DEFAULT NULL,
-  `total_time` time DEFAULT NULL
-) 
-
-
 CREATE TABLE `rider` (
   `rider_ID` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `rider_name` varchar(30) DEFAULT NULL,
@@ -18,11 +9,6 @@ CREATE TABLE `rider` (
 ) 
 
 
-CREATE TABLE `riderreport` (
-  `riderReport_ID` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `commission_ID` int(11) NOT NULL,
-  FOREIGN KEY (`commission_ID`) REFERENCES commission(`commission_ID`)
-)
 CREATE TABLE `Admin` (
   `admin_ID` int(10) NOT NULL,
   `user_ID` varchar(10) NOT NULL,
