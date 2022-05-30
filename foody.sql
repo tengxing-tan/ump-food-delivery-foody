2dc42e2d177c288e4a0bcf04caec59aa8e69905b
Loo Chang Herng <74412904+Herng19@users.noreply.github.com>
2022-05-30 09:59:28 +0800

    Insert user sql

--- foody.sql
+++ foody.sql
@@ -40,6 +40,51 @@ CREATE TABLE `User` (
   `user_address` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE `expensesrecord` (
+  `expenses_ID` int(10) NOT NULL,
+  `user_ID` int(10) NOT NULL,
+  `expenses_title` varchar(50) NOT NULL,
+  `expenses_description` varchar(200) NOT NULL,
+  `expenses_date` date NOT NULL,
+  `expenses_amount` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `order` (
+  `order_ID` int(10) NOT NULL,
+  `restaurant_ID` varchar(10) NOT NULL,
+  `rider_ID` varchar(10) NOT NULL,
+  `user_ID` varchar(10) NOT NULL,
+  `extra_note` varchar(100) NOT NULL,
+  `order_date` date NOT NULL,
+  `total_amount` int(11) NOT NULL,
+  `order_status` varchar(20) NOT NULL,
+  `order_time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `orderedfood` (
+  `order_ID` int(10) NOT NULL,
+  `restaurant_ID` int(10) NOT NULL,
+  `food_ID` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `restaurant` (
+  `restaurant_ID` int(10) NOT NULL,
+  `ro_ID` varchar(10) NOT NULL,
+  `restaurant_name` varchar(30) NOT NULL,
+  `restaurant_image` varchar(255) NOT NULL,
+  `restaurantType_ID` int(10) NOT NULL,
+  `restaurant_descripton` text NOT NULL,
+  `restaurant_address` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `restauranttype` (
+  `restaurantType_ID` int(10) NOT NULL,
+  `restaurantType_name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+
 ALTER TABLE `Admin`
   ADD PRIMARY KEY (`admin_ID`),
   ADD KEY `user_ID` (`user_ID`);
@@ -47,8 +92,86 @@ ALTER TABLE `Admin`
 ALTER TABLE `User`
   ADD PRIMARY KEY (`user_ID`);
 
+ALTER TABLE `expensesrecord`
+  ADD PRIMARY KEY (`expenses_ID`),
+  ADD KEY `user_ID` (`user_ID`);
+
+--
+-- Indexes for table `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`order_ID`);
+
+--
+-- Indexes for table `orderedfood`
+--
+ALTER TABLE `orderedfood`
+  ADD KEY `order_ID` (`order_ID`);
+
+--
+-- Indexes for table `restaurant`
+--
+ALTER TABLE `restaurant`
+  ADD PRIMARY KEY (`restaurant_ID`),
+  ADD KEY `restaurantType_ID` (`restaurantType_ID`);
+
+--
+-- Indexes for table `restauranttype`
+--
+ALTER TABLE `restauranttype`
+  ADD PRIMARY KEY (`restaurantType_ID`);
 
 ALTER TABLE `Admin`
   ADD CONSTRAINT `Admin_ibfk_1` FOREIGN KEY (`user_ID`) REFERENCES `User` (`user_ID`);
+  
+  ALTER TABLE `expensesrecord`
+  MODIFY `expenses_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `order`
+--
+ALTER TABLE `order`
+  MODIFY `order_ID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `restaurant`
+--
+ALTER TABLE `restaurant`
+  MODIFY `restaurant_ID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `restauranttype`
+--
+ALTER TABLE `restauranttype`
+  MODIFY `restaurantType_ID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `expensesrecord`
+--
+ALTER TABLE `expensesrecord`
+  ADD CONSTRAINT `user_ID` FOREIGN KEY (`user_ID`) REFERENCES `user` (`user_ID`);
+
+--
+-- Constraints for table `orderedfood`
+--
+ALTER TABLE `orderedfood`
+  ADD CONSTRAINT `order_ID` FOREIGN KEY (`order_ID`) REFERENCES `order` (`order_ID`);
+
+--
+-- Constraints for table `restaurant`
+--
+ALTER TABLE `restaurant`
+  ADD CONSTRAINT `restaurantType_ID` FOREIGN KEY (`restaurantType_ID`) REFERENCES `restauranttype` (`restaurantType_ID`);
+COMMIT;
 
 COMMIT;
