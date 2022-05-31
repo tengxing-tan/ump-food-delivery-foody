5f37f1df44282453d4ebabb016cafd866629081d
Loo Chang Herng <74412904+Herng19@users.noreply.github.com>
2022-05-31 21:14:12 +0800

    update foody.sql
    latest sql

--- foody.sql
+++ foody.sql
@@ -1,21 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 31, 2022 at 03:11 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
 
-CREATE TABLE `rider` (
-  `rider_ID` int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `rider_name` varchar(30) DEFAULT NULL,
-  `rider_email` varchar(30) DEFAULT NULL,
-  `rider_phoneNum` varchar(11) DEFAULT NULL,
-  `rider_password` varchar(20) DEFAULT NULL,
-  `rider_address` varchar(30) DEFAULT NULL
-) 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE `feedback` (
-  `feedback_ID` int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `id` int(10) NOT NULL,
-  `feedback_info` varchar(30) NULL,
-  `feedback_status` varchar(30) NULL,
-   FOREIGN KEY (`id`) REFERENCES complaintlist(`id`)
-) 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `foodydb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
 
 CREATE TABLE `admin` (
   `admin_ID` int(10) NOT NULL,
@@ -25,14 +35,28 @@ CREATE TABLE `admin` (
   `admin_password` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `user` (
-  `user_ID` int(10) NOT NULL,
-  `user_name` varchar(30) NOT NULL,
-  `user_email` varchar(30) NOT NULL,
-  `user_phoneNum` varchar(11) NOT NULL,
-  `user_password` varchar(20) NOT NULL,
-  `user_address` varchar(50) DEFAULT NULL
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complaintlist`
+--
+
+CREATE TABLE `complaintlist` (
+  `complaint_id` int(10) NOT NULL,
+  `order_id` int(10) NOT NULL,
+  `complaint_name` varchar(50) NOT NULL,
+  `complaint_date` date NOT NULL,
+  `complaint_time` time NOT NULL,
+  `complaint_type` varchar(50) NOT NULL,
+  `complaint_comment` varchar(200) NOT NULL,
+  `complaint_status` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `expensesrecord`
+--
 
 CREATE TABLE `expensesrecord` (
   `expenses_ID` int(10) NOT NULL,
@@ -42,6 +66,67 @@ CREATE TABLE `expensesrecord` (
   `expenses_date` date NOT NULL,
   `expenses_amount` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `feedback_id` int(10) NOT NULL,
+  `complaint_id` int(10) NOT NULL,
+  `feedback_info` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food`
+--
+
+CREATE TABLE `food` (
+  `food_ID` int(10) NOT NULL,
+  `restaurant_ID` int(10) NOT NULL,
+  `food_title` varchar(30) NOT NULL,
+  `food_category_ID` int(10) NOT NULL,
+  `food_description` text NOT NULL,
+  `food_image` varchar(255) NOT NULL,
+  `food_price` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `food`
+--
+
+INSERT INTO `food` (`food_ID`, `restaurant_ID`, `food_title`, `food_category_ID`, `food_description`, `food_image`, `food_price`) VALUES
+(13, 1, 'sushi', 1, 'Sushi', '../../src/img/sushi-mentai.jpg', 12);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `foodcategory`
+--
+
+CREATE TABLE `foodcategory` (
+  `food_category_ID` int(10) NOT NULL,
+  `category_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `foodcategory`
+--
+
+INSERT INTO `foodcategory` (`food_category_ID`, `category_name`) VALUES
+(1, 'Main Dishes'),
+(2, 'Side Dishes'),
+(3, 'Drinks');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
 
 CREATE TABLE `order` (
   `order_ID` int(10) NOT NULL,
@@ -55,12 +140,24 @@ CREATE TABLE `order` (
   `order_time` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderedfood`
+--
+
 CREATE TABLE `orderedfood` (
   `order_ID` int(10) NOT NULL,
   `restaurant_ID` int(10) NOT NULL,
-  `food_ID` int(10) NOT NULL, 
+  `food_ID` int(10) NOT NULL,
   `food_quantity` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `restaurant`
+--
 
 CREATE TABLE `restaurant` (
   `restaurant_ID` int(10) NOT NULL,
@@ -72,33 +169,137 @@ CREATE TABLE `restaurant` (
   `restaurant_address` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `restauranttype` (
-  `restaurantType_ID` int(10) NOT NULL,
-  `restaurantType_name` varchar(20) NOT NULL
+--
+-- Dumping data for table `restaurant`
+--
+
+INSERT INTO `restaurant` (`restaurant_ID`, `ro_ID`, `restaurant_name`, `restaurant_image`, `restaurantType_ID`, `restaurant_descripton`, `restaurant_address`) VALUES
+(1, '1', 'Sushi Mentai', '../src/img/sushi-mentai.jpg', 1, 'Sushi Restaurant', '9, Jalan Apa, Taman Apa, 65500 Pekan, Pahang');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `restaurantowner`
+--
+
+CREATE TABLE `restaurantowner` (
+  `ro_ID` int(10) NOT NULL,
+  `ro_name` varchar(30) NOT NULL,
+  `ro_email` varchar(30) NOT NULL,
+  `ro_phoneNum` varchar(15) NOT NULL,
+  `ro_password` varchar(20) NOT NULL,
+  `ro_address` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `restaurantowner`
+--
+
+INSERT INTO `restaurantowner` (`ro_ID`, `ro_name`, `ro_email`, `ro_phoneNum`, `ro_password`, `ro_address`) VALUES
+(1, 'tan teng xing', 'tengxing@@gmail.com', '123456', 'tan', 'tan teng xing\r\ntan teng xing\r\ntan teng xing');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `restauranttype`
+--
+
+CREATE TABLE `restauranttype` (
+  `restaurantType_ID` int(10) NOT NULL,
+  `restaurantType_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `restauranttype`
+--
+
+INSERT INTO `restauranttype` (`restaurantType_ID`, `restaurantType_name`) VALUES
+(1, 'Local Restaurant'),
+(2, 'Western Restaurant'),
+(3, 'Vegeterian Restaurant');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_ID` int(10) NOT NULL,
+  `user_name` varchar(30) NOT NULL,
+  `user_email` varchar(30) NOT NULL,
+  `user_phoneNum` varchar(11) NOT NULL,
+  `user_password` varchar(20) NOT NULL,
+  `user_address` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_ID`, `user_name`, `user_email`, `user_phoneNum`, `user_password`, `user_address`) VALUES
+(1, 'jeremy', 'jeremy@example.com', '010-2520223', 'jeremy', '21, Jalan Melaka, Taman Melaka, 65500 Pekan, Pahan');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`admin_ID`),
   ADD KEY `user_ID` (`user_ID`);
 
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_ID`);
+--
+-- Indexes for table `complaintlist`
+--
+ALTER TABLE `complaintlist`
+  ADD PRIMARY KEY (`complaint_id`),
+  ADD KEY `order_id` (`order_id`);
 
+--
+-- Indexes for table `expensesrecord`
+--
 ALTER TABLE `expensesrecord`
   ADD PRIMARY KEY (`expenses_ID`),
   ADD KEY `user_ID` (`user_ID`);
 
 --
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`feedback_id`),
+  ADD KEY `complaint_id` (`complaint_id`);
+
+--
+-- Indexes for table `food`
+--
+ALTER TABLE `food`
+  ADD PRIMARY KEY (`food_ID`),
+  ADD KEY `FK_RestaurantFood` (`restaurant_ID`),
+  ADD KEY `FK_FoodCategoryFood` (`food_category_ID`);
+
+--
+-- Indexes for table `foodcategory`
+--
+ALTER TABLE `foodcategory`
+  ADD PRIMARY KEY (`food_category_ID`);
+
+--
 -- Indexes for table `order`
 --
 ALTER TABLE `order`
-  ADD PRIMARY KEY (`order_ID`);
+  ADD PRIMARY KEY (`order_ID`),
+  ADD KEY `restaurant_ID` (`restaurant_ID`),
+  ADD KEY `user_ID` (`user_ID`);
 
 --
 -- Indexes for table `orderedfood`
 --
 ALTER TABLE `orderedfood`
-  ADD KEY `order_ID` (`order_ID`);
+  ADD KEY `order_ID` (`order_ID`),
+  ADD KEY `food_ID` (`food_ID`);
 
 --
 -- Indexes for table `restaurant`
@@ -108,116 +309,57 @@ ALTER TABLE `restaurant`
   ADD KEY `restaurantType_ID` (`restaurantType_ID`);
 
 --
+-- Indexes for table `restaurantowner`
+--
+ALTER TABLE `restaurantowner`
+  ADD PRIMARY KEY (`ro_ID`);
+
+--
 -- Indexes for table `restauranttype`
 --
 ALTER TABLE `restauranttype`
   ADD PRIMARY KEY (`restaurantType_ID`);
 
-ALTER TABLE `admin`
-  ADD CONSTRAINT `Admin_ibfk_1` FOREIGN KEY (`user_ID`) REFERENCES `User` (`user_ID`);
-  
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `complaintlist`
+--
+ALTER TABLE `complaintlist`
+  MODIFY `complaint_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `expensesrecord`
+--
 ALTER TABLE `expensesrecord`
   MODIFY `expenses_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
-
-CREATE TABLE `food` (
-  `food_ID` int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `restaurant_ID` int(10) NOT NULL,
-  `food_title` varchar(30) NOT NULL,
-  `food_category_ID` int(10) NOT NULL,
-  `food_description` text NOT NULL,
-  `food_image` varchar(255) NOT NULL,
-  `food_price` float NOT NULL,
-   CONSTRAINT FK_RestaurantFood FOREIGN KEY (`restaurant_ID`) REFERENCES Restaurant(`restaurant_ID`),
-   CONSTRAINT FK_FoodCategoryFood FOREIGN KEY (`food_category_ID`) REFERENCES FoodCategory(`food_category_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `feedback_id` int(10) NOT NULL AUTO_INCREMENT;
 
 --
--- Table structure for table `FoodCategory`
+-- AUTO_INCREMENT for table `food`
 --
-
-CREATE TABLE `foodcategory` (
-  `food_category_ID` int(10) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `category_name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
+ALTER TABLE `food`
+  MODIFY `food_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
--- Table structure for table `Restaurant`
+-- AUTO_INCREMENT for table `foodcategory`
 --
+ALTER TABLE `foodcategory`
+  MODIFY `food_category_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
-CREATE TABLE `restaurant` (
-  `restaurant_ID` int(10) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `ro_ID` int(10) NOT NULL   REFERENCES RestaurantOwner(`ro_ID`),
-  `restaurant_name` varchar(100) NOT NULL,
-  `restaurant_imgae` varchar(255) NOT NULL,
-  `restaurant_description` text NOT NULL,
-  `restaurant_address` varchar(100) NOT NULL,
-  CONSTRAINT FK_RestaurantOwnerRestaurant FOREIGN KEY (`ro_ID`) REFERENCES RestaurantOwner(`ro_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
---
--- Table structure for table `RestaurantOwner`
---
-
-CREATE TABLE `restaurantowner` (
-  `ro_ID` int(10) NOT NULL PRIMARY KEY,
-  `ro_name` varchar(30) NOT NULL,
-  `ro_email` varchar(30) NOT NULL,
-  `ro_phoneNum` varchar(15) NOT NULL,
-  `ro_password` varchar(20) NOT NULL,
-  `ro_address` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-
---
--- Dumping data for table `Food`
---
-
-INSERT INTO `food` (`food_ID`, `restaurant_ID`, `food_title`, `food_category_ID`, `food_description`, `food_image`, `food_price`) VALUES
-(NULL, 1, 'Asam Laksa', 1, '', 'asam_laksa.jpg', 9),
-(NULL, 1, 'Cendol', 2, 'update x2', 'cendol.jpg', 40),
-(NULL, 1, 'Fried Chicken', 2, '1px only', 'fried_chicken.jpg', 4),
-(NULL, 1, 'Nasi Kerabu', 1, '', 'nasi_kerabu.jpg', 10),
-(NULL, 1, 'Mee Sup', 1, '', 'mee_sup.jpg', 5),
-(NULL, 1, 'Milo Ais', 3, '', 'milo_ais.jpg', 2.5),
-(NULL, 1, 'Nasi Kerabu', 1, '', 'nasi_kerabu.jpg', 7.5),
-(NULL, 1, 'Nasi Lemak', 1, '', 'nasi_lemak.jpg', 5.5),
-(NULL, 1, 'Roti Canai', 2, '', 'roti_canai.jpg', 1.2),
-(NULL, 1, 'Roti Telur', 2, '', 'roti_telur.jpg', 2.5),
-(NULL, 1, 'Teh Tarik', 3, '', 'teh_tarik.jpg', 2),
-(NULL, 1, 'tempura', 2, '', 'tempura.png', 9.99);
-
--- --------------------------------------------------------
-
---
--- Dumping data for table `FoodCategory`
---
-
-INSERT INTO `foodcategory` (`food_category_ID`, `category_name`) VALUES
-(1, 'Main Dishes'),
-(2, 'Side Dishes'),
-(3, 'Drinks');
-
--- --------------------------------------------------------
-
---
--- Dumping data for table `Restaurant`
---
-
-INSERT INTO `restaurant` (`restaurant_ID`, `ro_ID`, `restaurant_name`, `restaurant_imgae`, `restaurant_description`, `restaurant_address`) VALUES
-(1, 1, 'hotdog', '../assets/tempura.png', 'no food', 'tan teng xing\r\ntan teng xing\r\ntan teng xing');
-
--- --------------------------------------------------------
-
---
--- Dumping data for table `RestaurantOwner`
---
-
-INSERT INTO `restaurantowner` (`ro_ID`, `ro_name`, `ro_email`, `ro_phoneNum`, `ro_password`, `ro_address`) VALUES
-(1, 'tan teng xing', 'tengxing@@gmail.com', '123456', 'tan', 'tan teng xing\r\ntan teng xing\r\ntan teng xing');
 --
 -- AUTO_INCREMENT for table `order`
 --
@@ -228,13 +370,13 @@ ALTER TABLE `order`
 -- AUTO_INCREMENT for table `restaurant`
 --
 ALTER TABLE `restaurant`
-  MODIFY `restaurant_ID` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `restaurant_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `restauranttype`
 --
 ALTER TABLE `restauranttype`
-  MODIFY `restaurantType_ID` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `restaurantType_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `user`
@@ -245,101 +387,45 @@ ALTER TABLE `user`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `complaintlist`
+--
+ALTER TABLE `complaintlist`
+  ADD CONSTRAINT `complaintlist_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `order` (`order_ID`);
+
+--
+-- Constraints for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`complaint_id`) REFERENCES `complaintlist` (`complaint_id`);
+
+--
+-- Constraints for table `food`
+--
+ALTER TABLE `food`
+  ADD CONSTRAINT `FK_FoodCategoryFood` FOREIGN KEY (`food_category_ID`) REFERENCES `foodcategory` (`food_category_ID`),
+  ADD CONSTRAINT `FK_RestaurantFood` FOREIGN KEY (`restaurant_ID`) REFERENCES `restaurant` (`restaurant_ID`);
+
 --
 -- Constraints for table `order`
 --
 ALTER TABLE `order`
-  ADD CONSTRAINT `restaurant_ID` FOREIGN KEY (`restaurant_ID`) REFERENCES `restaurant` (`restaurant_ID`);
-ALTER TABLE `order`
-  ADD CONSTRAINT `rider_ID` FOREIGN KEY (`rider_ID`) REFERENCES `rider` (`rider_ID`);
-ALTER TABLE `order`
-  ADD CONSTRAINT `user_ID` FOREIGN KEY (`user_ID`) REFERENCES `user` (`user_ID`);
---
--- Constraints for table `expensesrecord`
---
-ALTER TABLE `expensesrecord`
+  ADD CONSTRAINT `restaurant_ID` FOREIGN KEY (`restaurant_ID`) REFERENCES `restaurant` (`restaurant_ID`),
   ADD CONSTRAINT `user_ID` FOREIGN KEY (`user_ID`) REFERENCES `user` (`user_ID`);
 
 --
 -- Constraints for table `orderedfood`
 --
 ALTER TABLE `orderedfood`
+  ADD CONSTRAINT `food_ID` FOREIGN KEY (`food_ID`) REFERENCES `food` (`food_ID`),
   ADD CONSTRAINT `order_ID` FOREIGN KEY (`order_ID`) REFERENCES `order` (`order_ID`);
-  
-ALTER TABLE `orderedfood`
-  ADD CONSTRAINT `restaurant_ID` FOREIGN KEY (`restaurant_ID`) REFERENCES `restaurant` (`restaurant_ID`);
-  
-ALTER TABLE `orderedfood`
-  ADD CONSTRAINT `food_ID` FOREIGN KEY (`food_ID`) REFERENCES `food` (`food_ID`);
 
 --
 -- Constraints for table `restaurant`
 --
 ALTER TABLE `restaurant`
   ADD CONSTRAINT `restaurantType_ID` FOREIGN KEY (`restaurantType_ID`) REFERENCES `restauranttype` (`restaurantType_ID`);
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Oct 06, 2017 at 06:48 AM
--- Server version: 10.1.26-MariaDB
--- PHP Version: 7.1.9
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `complaintlist`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `complaintlist`
---
-
-CREATE TABLE `complaintlist` (
-  `id` int(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  `complaint_name` varchar(50) NOT NULL,
-  `complaint_date` date NOT NULL,
-  `complaint_time` time NOT NULL,
-  `complaint_type` varchar(50) NOT NULL,
-  `complaint_comment` varchar(200) NOT NULL,
-  `complaint_status` varchar(30) NOT NULL,
-  `feedback_info` varchar(30) NULL,
-  `feedback_status` varchar(30) NULL,
-   FOREIGN KEY (`order_ID`) REFERENCES order(`order_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `complaintlist`
---
-
-ALTER TABLE `complaintlist`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `complaintlist`
---
-ALTER TABLE `complaintlist`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
