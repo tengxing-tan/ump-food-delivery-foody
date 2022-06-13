cbeb5068062fe1b6ceb5ab647bff82466a64e82d
Tan Teng Xing <cb20121@student.ump.edu.my>
2022-06-13 22:49:24 +0800

    link admin complaint

--- admin/ComplaintReport.php
+++ admin/ComplaintReport.php
@@ -1,18 +1,3 @@
 <?php
-    include_once 'dbconnection.php';
-    include_once 'navBar.php';
-    session_start();
+    header('Location: ../complaint/foody-complaint/admin/report.php')
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    complaint module
-</body>
-</html>
