875b85a7329524c09b564934be3abc59cb796a69
Tan Teng Xing <cb20121@student.ump.edu.my>
2022-06-13 21:10:44 +0800

    change db connection to remote

--- complaint/foody-complaint/connecttest.php
+++ complaint/foody-complaint/connecttest.php
@@ -1,10 +1,23 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "");
+// $conn = mysqli_connect("localhost", "root", "");
+
+/**
+ * Get Heroku ClearDB connection information
+ */
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+/* Connect to DB */
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-mysqli_select_db($conn, "foodydb") or die("Could not open profile database");
+// mysqli_select_db($conn, "foodydb") or die("Could not open profile database");
 
 date_default_timezone_set('Asia/Kuala_Lumpur');
--- rider/view/connectDB.php
+++ rider/view/connectDB.php
@@ -1,19 +1,19 @@
 <?php
-$link = mysqli_connect("localhost", "root", "");
+// $link = mysqli_connect("localhost", "root", "");
 
-mysqli_select_db($link, "foodydb") or die(mysqli_connect_error());
+// mysqli_select_db($link, "foodydb") or die(mysqli_connect_error());
 
 /**
  * Get Heroku ClearDB connection information
  */
-// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-// $cleardb_server = $cleardb_url["host"];
-// $cleardb_username = $cleardb_url["user"];
-// $cleardb_password = $cleardb_url["pass"];
-// $cleardb_db = substr($cleardb_url["path"],1);
-// $active_group = 'default';
-// $query_builder = TRUE;
-// /* Connect to DB */
-// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+/* Connect to DB */
+$link = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 ?>
