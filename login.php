745f6f68d2f3877ef19459f059ab3a607e0b4dba
Tan Teng Xing <cb20121@student.ump.edu.my>
2022-06-13 14:05:44 +0800

    update login css

--- login.php
+++ login.php
@@ -73,41 +73,111 @@
         }
     }
 
-
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            min-height: 100vh;
+            padding: 0;
+            margin: 0;
+            background: rgb(108, 90, 138);
+            background: radial-gradient(circle, rgba(86, 195, 203, 0.4) 0%, rgba(108, 90, 138, 0.7) 80%);
+        }
+
+        .login-form {
+            display: flex;
+            flex-direction: column;
+            align-items: stretch;
+
+            background-color: whitesmoke;
+            width: 20rem;
+            padding: 3rem;
+            border-radius: 24px;
+            font: 14px sans-serif;
+            z-index: 10;
+            box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
+        }
+
+        label {
+            color: #4e4e4e;
+            font-weight: bold;
+            display: inline-block;
+            width: 7rem;
+            margin-bottom: 0.25rem;
+        }
+
+        .input {
+            display: block;
+            padding: 0.5rem 1.25rem;
+            margin-bottom: 1rem;
+            border-radius: 8px;
+            border: 2px solid #e8e8e8;
+            font: inherit;
+            outline-width: 2px;
+        }
+
+        .input:focus {
+            outline: #555 solid 2px;
+        }
+
+        button[type=submit] {
+            display: block;
+            appearance: none;
+            background-color: #6C5A8A;
+            color: white;
+            padding: 0.75rem 0;
+            margin-top: 1.5rem;
+            border-radius: 8px;
+            font: bold 1rem sans-serif;
+        }
+
+        #illustration-delivery {
+            position: absolute;
+            width: 350px;
+            height: 150px;
+            right: 10%;
+            top: 55%;
+        }
+    </style>
 </head>
+
 <body>
+    <img id="illustration-delivery" src="assets/img/undraw1.png">
 
-<h1>Foody</h1>  
-<h2>Welcome back!</h2>
-    <form action="#" method="post">
+    <form class="login-form" action="#" method="post">
+        <div>
+            <h1 style="text-align: center;">Foody Login</h1>
+            <h2 style="text-align: center; color: #4a4a4a; font-size: 1.15rem;">Welcome back!</h2>
+        </div>
+        <label for="email">Email:</label>
+        <input class="input" type="email" name="email" id="email" autocomplete="email" placeholder="Email address">
 
-          <label for="email">Email:</label>  
-          <input type="email" name="email" id="email" ><br><br>
-    
-          <label for="fpassword">Password:</label>   
-        <input type="password" name="password" id="fpassword" ><br><br>
+        <label for="fpassword">Password:</label>
+        <input class="input" type="password" name="password" id="fpassword" autocomplete="password" placeholder="Password">
 
-        <select name="type">
+        <select class="input" name="type">
             <option disable selected value>Choose User Type</option>
             <option value="General User">User</option>
             <option value="Admin">Administrator</option>
             <option value="Rider">Rider</option>
             <option value="Restaurant Owner">Restaurant Owner</option>
         </select>
-      
-       
-          <button type="submit" name="Login" value = "Login" >Submit</button>                 
+
+        <button class="btn" type="submit" name="Login" value="Login">Login</button>
     </form>
-    
+
 </body>
+
 </html>
